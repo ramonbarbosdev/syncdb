77a46d0d52f3948c5f3ac8825fd7258fd83c9da0
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-05-19 15:25:04 -0300

    ajusta mensagens de erro e formatação no script docker-start.bat; adiciona verificação de instalação do Docker e Docker Compose

--- docker-start.bat
+++ docker-start.bat
@@ -1,38 +1,39 @@
 @echo off
 
+:: Verifica se Docker está instalado
 docker --version >nul 2>nul
 if %ERRORLEVEL% neq 0 (
-    echo "Docker não encontrado. Certifique-se de que o Docker esteja instalado."
+    echo Docker não encontrado. Certifique-se de que o Docker esteja instalado.
     pause
     exit /b
 )
 
+:: Verifica se Docker Compose está instalado
 docker-compose --version >nul 2>nul
 if %ERRORLEVEL% neq 0 (
-    echo "Docker Compose não encontrado. Certifique-se de que o Docker Compose esteja instalado."
+    echo Docker Compose não encontrado. Certifique-se de que o Docker Compose esteja instalado.
     pause
     exit /b
 )
 
+:: Configurações
 set REPOSITORY=ramonbarbosdev
-
 set IMAGE_NAME=syncdb
 set TAG=latest
+set PROJECT_NAME=docker-compose
 
-echo "Derrubando os containers existentes..."
-docker-compose down
-
-echo "Recriando e atualizando os containers..."
-docker-compose up -d
-
-
-echo "Construindo a imagem Docker..."
+echo.
+echo === Construindo a imagem Docker ===
 docker build -t %REPOSITORY%/%IMAGE_NAME%:%TAG% .
 
-docker run -d --name %IMAGE_NAME% %REPOSITORY%/%IMAGE_NAME%:%TAG%
+echo.
+echo === Subindo containers com Docker Compose ===
+docker-compose -p %PROJECT_NAME% up -d --build
 
-echo "Enviando a imagem para o Docker Hub..."
+echo.
+echo === Enviando a imagem para o Docker Hub ===
 docker push %REPOSITORY%/%IMAGE_NAME%:%TAG%
 
-echo "Processo concluído!"
+echo.
+echo === Processo concluído com sucesso! ===
 pause
